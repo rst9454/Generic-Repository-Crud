0e4b4782ce3e4ffe49928fa12df42b3fc58640d8
SUNIL PANDEY <sunilcid763@gmail.com>
2024-08-18 16:26:41 +0530

    Generic Service Implementation

---
+++ GenericSerivce/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace Using_GenericRepository_CrudOperation.GenericSerivce
+{
+    public interface IGenericService
+    {
+        Task<T> GetByIdAsync<T>(int id) where T : class;
+        Task<IEnumerable<T>> GetAllAsync<T>() where T : class;
+        Task AddAsync<T>(T entity) where T : class;
+        Task UpdateAsync<T>(T entity) where T : class;
+        Task DeleteAsync<T>(int id) where T : class;
+    }
+}
